c55aff90b854b192cc1a549b52656d333cd578f8
Victoria Laughlin <victoria.laughlin@gmail.com>
2023-08-16 12:47:04 -0400

    header updated
    made it so header is not static

--- client/index.js
+++ client/index.js
@@ -1,38 +1,47 @@
-function Book(title, author, pages, description,is_read){
+function Book(id, title, author, pages, description, shelfNum, genre, favorite){
+  this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.description = description;
-  this.is_read = is_read;
+  this.shelfNum = shelfNum;
+  this.genre = genre;
+  this.favorite = favorite;
   
 }
-
-
 
 function addBookToLibrary(library, book){
   library.push(book);
 }
 
 function createTable(array){
-  const list = document.getElementById("list-books");
-  
-  for (let i = 0; i < array.length; i++){
-    // Each row lists a book
-    const row = document.createElement("tr");
-    const bookArray = Object.values(array[i]);
-    let cell = document.createElement("td");
-    let cellText = document.createTextNode(i+1);
-    cell.appendChild(cellText);
-    row.appendChild(cell);
-    
+  const thead = document.getElementById("list-header");
+  const tbody = document.getElementById("list-body");
+  let row = document.createElement("tr");
+  const headerArray = Object.keys(array[0]);
+
+  // List Header onto the table
+  for(let i=0; i< headerArray.length; i++){
+    const header = document.createElement("th");
+    const headerText = document.createTextNode(headerArray[i]);
+    header.appendChild(headerText);
+    row.appendChild(header);
+  }
+  thead.appendChild(row);
+ 
+  // List Books onto the table using 2 for loops. First adds book values into another array, the second lists out the new array
+  for (let j = 0; j < array.length; j++){
+     const bookArray = Object.values(array[j]);
+     //reset row element
+     row = document.createElement("tr");
     // List each column
-    for(let j = 0; j< bookArray.length; j++){
-      cell = document.createElement("td");
-      const cellText = document.createTextNode(bookArray[j]);
+    for(let k = 0; k < bookArray.length; k++){
+      const cell = document.createElement("td");
+      const cellText = document.createTextNode(bookArray[k]);
       cell.appendChild(cellText);
       row.appendChild(cell);
     }
-    list.appendChild(row);
+    tbody.appendChild(row);
   }
 }
 
@@ -42,15 +51,11 @@ Book.prototype = this.info = function(){
 
 const myLibrary = [];
 const list = document.getElementById("list-books");
-const book1 = new Book('Harry Potter', 'JK Rowling', '233', 'About witches and wizards...', false);
-const book2 = new Book('Lord of the Rings', 'JRR Tolkien', '432', 'A hobbit brings the ring to Mordor...', true);
+const book1 = new Book(1, "Harry Potter and the Philosopher's Stone", 'JK Rowling', '233', 'About witches and wizards...', '000.0 AA00 2005', "Children's Fantasy", false);
+const book2 = new Book(2, 'Lord of the Rings', 'JRR Tolkien', '432', 'A hobbit brings the ring to Mordor...', '000.0 AA00 2005','Science Fiction/Fantasy', true);
 
 addBookToLibrary(myLibrary, book1);
 addBookToLibrary(myLibrary, book2);
   
-for(i=0; i<myLibrary.length; i++){
-  console.log(myLibrary[i]);
-}
-
 createTable(myLibrary);
 
