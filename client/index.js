d1279c36cc6d8c976d2c38fe123042856e84e5ce
Victoria Laughlin <victoria.laughlin@gmail.com>
2023-08-16 23:35:47 -0400

    v0.1.0

--- client/index.js
+++ client/index.js
@@ -7,20 +7,16 @@ function Book(id, title, author, pages, description, shelfNum, genre, favorite){
   this.shelfNum = shelfNum;
   this.genre = genre;
   this.favorite = favorite;
-  
-}
-
-function addBookToLibrary(library, book){
-  library.push(book);
 }
 
 function createTable(array){
   const thead = document.getElementById("list-header");
-  const tbody = document.getElementById("list-body");
+  // creates a general book object to generate header names
+  const headerBook = new Book();
   let row = document.createElement("tr");
-  const headerArray = Object.keys(array[0]);
-
-  // List Header onto the table
+  const headerArray = Object.keys(headerBook);
+  
+  // List Header names onto the table
   for(let i=0; i< headerArray.length; i++){
     const header = document.createElement("th");
     const headerText = document.createTextNode(headerArray[i]);
@@ -28,34 +24,58 @@ function createTable(array){
     row.appendChild(header);
   }
   thead.appendChild(row);
- 
-  // List Books onto the table using 2 for loops. First adds book values into another array, the second lists out the new array
+  
+  // List Books onto the table using 2 for loops. First loop adds book values into another array, the second lists out the new array (using addBookToLibrary)
   for (let j = 0; j < array.length; j++){
-     const bookArray = Object.values(array[j]);
-     //reset row element
-     row = document.createElement("tr");
-    // List each column
-    for(let k = 0; k < bookArray.length; k++){
-      const cell = document.createElement("td");
-      const cellText = document.createTextNode(bookArray[k]);
-      cell.appendChild(cellText);
-      row.appendChild(cell);
-    }
-    tbody.appendChild(row);
+    //reset row element
+    addBookToLibrary(array[j]);
   }
 }
 
-Book.prototype = this.info = function(){
-  return title+" by "+author+", "+pages+", "+description+", "+is_read;
+function addBookToLibrary(book){
+  const bookArray = Object.values(book);
+  const tbody = document.getElementById("list-body");
+  let row = document.createElement("tr");
+  // List each column for book
+  for(let k = 0; k < bookArray.length; k++){
+    const cell = document.createElement("td");
+    const cellText = document.createTextNode(bookArray[k]);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+  }
+  tbody.appendChild(row);
+  
 }
 
 const myLibrary = [];
-const list = document.getElementById("list-books");
-const book1 = new Book(1, "Harry Potter and the Philosopher's Stone", 'JK Rowling', '233', 'About witches and wizards...', '000.0 AA00 2005', "Children's Fantasy", false);
-const book2 = new Book(2, 'Lord of the Rings', 'JRR Tolkien', '432', 'A hobbit brings the ring to Mordor...', '000.0 AA00 2005','Science Fiction/Fantasy', true);
-
-addBookToLibrary(myLibrary, book1);
-addBookToLibrary(myLibrary, book2);
-  
+let addBookForm = document.getElementById('addBookForm');
 createTable(myLibrary);
+
+// listens for submit on bootstrap modal and grabs field text
+addBookForm.addEventListener("submit", function(event){
+  let addBookForm = document.getElementById('addBookForm');
+  let title = document.getElementById("book-title");
+  let author = document.getElementById("book-author");
+  let pages = document.getElementById("book-pages");
+  let desc = document.getElementById("book-desc");
+  let shelf = document.getElementById("book-shelf");
+  let genre = document.getElementById("book-genre"); 
+  let favorite = false;
+
+  if(title.value == "" || author.value == "" ||  pages.value == "" || desc.value == "" || shelf.value == "" || genre.value == ""){
+    alert("Fields need to have text to submit!");
+  } else{
+    let book = new Book(myLibrary.length+1, title.value, author.value, pages.value, desc.value, shelf.value, genre.value, favorite);
+    addBookToLibrary(book);
+    myLibrary.push(book);
+    addBookForm.reset();
+    alert("New book "+title.value+" has been submitted!");    
+  }
+  event.preventDefault();
+});
+
+
+
+
+
 
